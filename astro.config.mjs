ccf43cd14978223017a6412cf26b1326703b79f6
Borja Vicente <nireneko@gmail.com>
2026-02-16 21:05:21 +0100

    Add wrangler to deploy on CloudFlare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: cloudflare()
 });
